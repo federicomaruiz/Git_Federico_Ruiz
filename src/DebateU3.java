ebcd036e32f3ff17eb5e69e2c462e7596a6c5558
FedeRuiz <federicomaruiz@gmail.com>
2023-01-20 00:05:33 -0300

    primer commit

---
+++ src/DebateU3.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author federicoruiz
+ * 20 ene 2023 0:04:40
+ */
+public class DebateU3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alumno: Federico_Ruiz");
+		
+	}
+
+}
